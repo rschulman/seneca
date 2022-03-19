6c83bbc0cddc579529504109eaaf8565b41aef81
Ross Schulman <ross@rbs.io>
2022-03-19 10:49:56 -0400

    Add unread indicator to thread list

--- src/ui/thread_list.rs
+++ src/ui/thread_list.rs
@@ -1,7 +1,8 @@
+use crate::{ui::virt_list::VirtList, MailData, Thread};
 use crate::{LOAD_THREAD, THREAD_BACKGROUND_COLOR};
-use crate::{MailData, Thread, ui::virt_list::VirtList};
 use chrono::Local;
-use druid::widget::{prelude::*, Container};
+use druid::kurbo::Circle;
+use druid::widget::{prelude::*, Container, Painter};
 use druid::widget::{CrossAxisAlignment, Flex, Label, WidgetExt};
 use druid::{Color, Insets, TextAlignment};
 use itertools::Itertools;
@@ -11,11 +12,28 @@ pub fn thread_list() -> impl Widget<MailData> {
     Flex::column()
         .cross_axis_alignment(CrossAxisAlignment::Start)
         .with_flex_child(
-                VirtList::vertical(THREAD_HEIGHT, || {
-                    Container::new(
-                        Flex::row()
-                            .cross_axis_alignment(CrossAxisAlignment::Start)
-                            .with_flex_child(Container::new(
+            VirtList::vertical(THREAD_HEIGHT, || {
+                Container::new(
+                    Flex::row()
+                        .cross_axis_alignment(CrossAxisAlignment::Start)
+                        .with_child(
+                            druid::widget::SizedBox::new(Painter::new(
+                                |ctx, data: &Thread, _env| {
+                                    let bounds = ctx.size().to_rect();
+                                    let radius = bounds.height() * 0.1;
+                                    if data.tags.contains(&"unread".to_string()) {
+                                        ctx.fill(
+                                            Circle::new(bounds.center(), radius),
+                                            &Color::rgb8(20, 217, 235),
+                                        );
+                                    }
+                                },
+                            ))
+                            .width(THREAD_HEIGHT * 0.6)
+                            .height(THREAD_HEIGHT),
+                        )
+                        .with_flex_child(
+                            Container::new(
                                 Flex::column()
                                     .cross_axis_alignment(CrossAxisAlignment::Start)
                                     .with_child(
@@ -25,7 +43,7 @@ pub fn thread_list() -> impl Widget<MailData> {
                                         .with_text_color(Color::BLACK)
                                         .with_font(crate::UI_FONT)
                                         .with_text_alignment(TextAlignment::Start)
-                                        .padding(Insets::new(10., 10., 10., 0.))
+                                        .padding(Insets::new(10., 10., 10., 0.)),
                                     )
                                     .with_default_spacer()
                                     .with_child(
@@ -36,33 +54,37 @@ pub fn thread_list() -> impl Widget<MailData> {
                                         .with_text_alignment(TextAlignment::Start)
                                         .with_font(crate::UI_FONT_LIGHT)
                                         .padding(Insets::new(10., 0., 10., 10.)),
-                                    )).fix_width(300.),
-                                0.85,
+                                    ),
                             )
-                            .with_default_spacer()
-                            .with_child(
-                                Label::new(|mail: &Thread, _env: &Env| {
-                                    let hours_ago = Local::now().signed_duration_since(*mail.date).num_hours();
-                                    if hours_ago < 24 {
-                                        format!("{} hours ago", hours_ago)
-                                    } else {
-                                        format!("{}", mail.date.format("%B %e, %Y"))
-                                    }
-                                    })
-                                    .with_text_color(Color::BLACK)
-                                    .with_font(crate::UI_FONT_LIGHT)
-                                    .with_text_size(11.0)
-                                    .with_text_alignment(TextAlignment::Start)
-                                        .padding(Insets::new(10., 10., 10., 10.)),
-                            )
-                            .must_fill_main_axis(true)
-                            .background(THREAD_BACKGROUND_COLOR)
-                            .on_click(|ctx, data: &mut Thread, _env| ctx.submit_command(LOAD_THREAD.with(data.clone()))
-                            ),
-                    )
-                    .background(THREAD_BACKGROUND_COLOR)
-                    .fix_height(THREAD_HEIGHT)
-                })
+                            .fix_width(300.),
+                            0.85,
+                        )
+                        .with_default_spacer()
+                        .with_child(
+                            Label::new(|mail: &Thread, _env: &Env| {
+                                let hours_ago =
+                                    Local::now().signed_duration_since(*mail.date).num_hours();
+                                if hours_ago < 24 {
+                                    format!("{} hours ago", hours_ago)
+                                } else {
+                                    format!("{}", mail.date.format("%B %e, %Y"))
+                                }
+                            })
+                            .with_text_color(Color::BLACK)
+                            .with_font(crate::UI_FONT_LIGHT)
+                            .with_text_size(11.0)
+                            .with_text_alignment(TextAlignment::Start)
+                            .padding(Insets::new(10., 10., 10., 10.)),
+                        )
+                        .must_fill_main_axis(true)
+                        .background(THREAD_BACKGROUND_COLOR)
+                        .on_click(|ctx, data: &mut Thread, _env| {
+                            ctx.submit_command(LOAD_THREAD.with(data.clone()))
+                        }),
+                )
+                .background(THREAD_BACKGROUND_COLOR)
+                .fix_height(THREAD_HEIGHT)
+            })
             .lens(MailData::threads),
             1.0,
         )
